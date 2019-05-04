9f9c9258ab988cfd2002db8b52af309de93aa9c9
neshimbekova <neshimbekova@gmail.com>
2019-05-04 14:43:26 -0500

    dsgfdhg

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,6 +5,7 @@ node{
     }
 
     stage("Install Requirements"){
+      sh "ssh ec2-user@${ENV} sudo pip install virtualenv"
       sh "scp -r * ec2-user@${ENV}:/tmp"
       sh "ssh ec2-user@${ENV} virtualenv /tmp/venv"
       sh "ssh ec2-user@${ENV} ./tmp/venv/activate"
